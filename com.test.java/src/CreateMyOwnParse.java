9c1ff700c7d7908c99592243396f95054aef67c8
NikaJan <v.milchakova9887@gmail.com>
2018-02-05 13:10:14 +0300

    hell

--- com.test.java/src/CreateMyOwnParse.java
+++ com.test.java/src/CreateMyOwnParse.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 /**
  * Created by veronika on 05.02.18.
  */
 public class CreateMyOwnParse {
+
+    public static void main(String[] args){
+        parseInt("543210");
+    }
+
+    public static int parseInt(String s){
+        int parsedValue = 0;
+            if((s != null)&&(!s.equals(""))&&(s.length()<=7)){
+                for(int i = 1; i <= s.length(); ++i){
+                    int counter = (int)Math.pow(1,i);
+                    i=i*10;
+                    System.out.println(counter);
+
+                    //System.out.println(s.charAt(4));
+                    //System.out.println(s.length());
+                }
+            }
+        return parsedValue;
+    }
+
 }
+
+
